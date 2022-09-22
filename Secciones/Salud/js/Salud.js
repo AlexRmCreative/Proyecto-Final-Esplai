3622fb2f00cb60dd589dcc848d549971bd145d32
Jean Pierre <jmmelono@gmail.com>
2022-09-22 07:03:50 +0200

    llamada de la API de la sección ayuda

--- Secciones/Salud/js/Salud.js
+++ Secciones/Salud/js/Salud.js
@@ -4,18 +4,23 @@ const descriptions = [];
 const url = "http://localhost:9090/section/5/media";
 
 async function fetchMensajesJSON() {
-  const response = await fetch(url);
-  const jsonResponse = await response.json();
-  return jsonResponse;
+    const response = await fetch(url);
+    const jsonResponse = await response.json();
+    return jsonResponse;
 }
 
 fetchMensajesJSON().then((json) => {
-  for (const element of json.media) {
+    for (const element of json.media) {
     mensajesSalud.push(element.urls);
     descriptions.push(element.description);
-  }
+    }
 
-  document.getElementById("salud-1").innerText=descriptions[0];
+    document.getElementById("salud-0").innerText=descriptions[0];
+    document.getElementById("salud-1").innerText=descriptions[6];
+    document.getElementById("salud-2").innerText=descriptions[2];
+    document.getElementById("salud-3").innerText=descriptions[17];
+    document.getElementById("salud-4").innerText=descriptions[13];
+    document.getElementById("salud-5").innerText=descriptions[10];
 });
 
 var maxNotiNumber = [0,0,0,0,0,0];
@@ -26,35 +31,35 @@ function generarNotificacion(idType = ""){
     var notificador = document.querySelectorAll(".notifySpace"); //4
     let ids = ["Alimentacion", "Ejercicio", "Mindfulness", "Yoga", "Tecnologia", "Introduccion"];
     
-    if(idType == ids[0] && maxNotiNumber[0] <= 5)
+    if(idType == ids[0] && maxNotiNumber[0] <= 4)
     {
         maxNotiNumber[0]++;
         setTimeout(() => {
             maxNotiNumber[0]--;
         }, 12000);
     }
-    if(idType == ids[1] && maxNotiNumber[1] <= 5)
+    if(idType == ids[1] && maxNotiNumber[1] <= 4)
     {
         maxNotiNumber[1]++;
         setTimeout(() => {
             maxNotiNumber[1]--;
         }, 12000);
     }
-    if(idType == ids[2] && maxNotiNumber[2] <= 5)
+    if(idType == ids[2] && maxNotiNumber[2] <= 2)
     {
         maxNotiNumber[2]++;
         setTimeout(() => {
             maxNotiNumber[2]--;
         }, 12000);
     }
-    if(idType == ids[3] && maxNotiNumber[3] <= 5)
+    if(idType == ids[3] && maxNotiNumber[3] <= 4)
     {
         maxNotiNumber[3]++;
         setTimeout(() => {
             maxNotiNumber[3]--;
         }, 12000);
     }
-    if(idType == ids[4] && maxNotiNumber[4] <= 5)
+    if(idType == ids[4] && maxNotiNumber[4] <= 3)
     {
         maxNotiNumber[4]++;
         setTimeout(() => {
@@ -71,7 +76,8 @@ function generarNotificacion(idType = ""){
 
 
     for (const element of notificador) {
-        if(maxNotiNumber[0] <= 5){
+        //Alimentación
+        if(maxNotiNumber[0] <= 4){
             if(idType == ids[0]){
                 if(element.id == ids[0]){
                     const divNotificacion = document.createElement('div');
@@ -88,11 +94,11 @@ function generarNotificacion(idType = ""){
                         }
                     }, 12000);
                     switch(notificacionNumber[0]){
-                        case 0: parrafoContenido.textContent = console.log(mensajesSalud[0]); break;
-                        case 1: parrafoContenido.textContent = "Y me gusta el chocolate"; break;
-                        case 2: parrafoContenido.textContent = "Pasear me despeja la mente"; break;
-                        case 3: parrafoContenido.textContent = "Y bailar menea mi cocotan"; break;
-                        case 4: parrafoContenido.textContent = "LO QUE ME GUSTA PERREAAAAAAR"; break;
+                        case 0: parrafoContenido.textContent = mensajesSalud[6]; break;
+                        case 1: parrafoContenido.textContent = mensajesSalud[7]; break;
+                        case 2: parrafoContenido.textContent = mensajesSalud[8]; break;
+                        case 3: parrafoContenido.textContent = mensajesSalud[9]; break;
+                        // case 4: parrafoContenido.textContent = mensajesSalud[2]; break;
                     }
                     element.appendChild(divNotificacion);
                     divNotificacion.appendChild(parrafoContenido);
@@ -109,7 +115,7 @@ function generarNotificacion(idType = ""){
         }
 
         //Ejercicio
-        if(maxNotiNumber[1] <= 5){
+        if(maxNotiNumber[1] <= 4){
             if(idType == ids[1]){
                 if(element.id == ids[1]){
                     const divNotificacion = document.createElement('div');
@@ -125,9 +131,9 @@ function generarNotificacion(idType = ""){
                     }, 12000);
                     switch(notificacionNumber[1]){
                         case 0: parrafoContenido.textContent = mensajesSalud[2]; break;
-                        case 1: parrafoContenido.textContent = console.log(mensajesSalud[3]); break;
-                        case 2: parrafoContenido.textContent = console.log(mensajesSalud[4]); break;
-                        case 3: parrafoContenido.textContent = console.log(mensajesSalud[5]); break;
+                        case 1: parrafoContenido.textContent = mensajesSalud[3]; break;
+                        case 2: parrafoContenido.textContent = mensajesSalud[4]; break;
+                        case 3: parrafoContenido.textContent = mensajesSalud[5]; break;
                     }
                     element.appendChild(divNotificacion);
                     divNotificacion.appendChild(parrafoContenido);
@@ -144,7 +150,7 @@ function generarNotificacion(idType = ""){
         }
 
         //Mindfulness
-        if(maxNotiNumber[2] <= 5){
+        if(maxNotiNumber[2] <= 2){
             if(idType == ids[2]){
                 if(element.id == ids[2]){
                     const divNotificacion = document.createElement('div');
@@ -159,11 +165,11 @@ function generarNotificacion(idType = ""){
                         }
                     }, 12000);
                     switch(notificacionNumber[2]){
-                        case 0: parrafoContenido.textContent = console.log(mensajesSalud[2]); break;
-                        case 1: parrafoContenido.textContent = console.log(mensajesSalud[2]); break;
-                        case 2: parrafoContenido.textContent = console.log(mensajesSalud[2]); break;
-                        case 3: parrafoContenido.textContent = console.log(mensajesSalud[2]); break;
-                        case 4: parrafoContenido.textContent = console.log(mensajesSalud[2]); break;
+                        case 0: parrafoContenido.textContent = mensajesSalud[17]; break;
+                        case 1: parrafoContenido.textContent = mensajesSalud[18]; break;
+                        // case 2: parrafoContenido.textContent = console.log(mensajesSalud[2]); break;
+                        // case 3: parrafoContenido.textContent = console.log(mensajesSalud[2]); break;
+                        // case 4: parrafoContenido.textContent = console.log(mensajesSalud[2]); break;
                     }
                     element.appendChild(divNotificacion);
                     divNotificacion.appendChild(parrafoContenido);
@@ -180,7 +186,7 @@ function generarNotificacion(idType = ""){
         }
 
         //Yoga
-        if(maxNotiNumber[3] <= 5){
+        if(maxNotiNumber[3] <= 4){
             if(idType == ids[3]){
                 if(element.id == ids[3]){
                     const divNotificacion = document.createElement('div');
@@ -195,11 +201,11 @@ function generarNotificacion(idType = ""){
                         }
                     }, 12000);
                     switch(notificacionNumber[3]){
-                        case 0: parrafoContenido.textContent = "Me llamo manolo"; break;
-                        case 1: parrafoContenido.textContent = "Y me gusta el chocolate"; break;
-                        case 2: parrafoContenido.textContent = "Pasear me despeja la mente"; break;
-                        case 3: parrafoContenido.textContent = "Y bailar menea mi cocotan"; break;
-                        case 4: parrafoContenido.textContent = "LO QUE ME GUSTA PERREAAAAAAR"; break;
+                        case 0: parrafoContenido.textContent = mensajesSalud[13]; break;
+                        case 1: parrafoContenido.textContent = mensajesSalud[14]; break;
+                        case 2: parrafoContenido.textContent = mensajesSalud[15]; break;
+                        case 3: parrafoContenido.textContent = mensajesSalud[16]; break;
+                        // case 4: parrafoContenido.textContent = "LO QUE ME GUSTA PERREAAAAAAR"; break;
                     }
                     element.appendChild(divNotificacion);
                     divNotificacion.appendChild(parrafoContenido);
@@ -216,7 +222,7 @@ function generarNotificacion(idType = ""){
         }
 
         //Tecnologia
-        if(maxNotiNumber[4] <= 5){
+        if(maxNotiNumber[4] <= 3){
             if(idType == ids[4]){
                 if(element.id == ids[4]){
                     const divNotificacion = document.createElement('div');
@@ -230,12 +236,12 @@ function generarNotificacion(idType = ""){
                             case 4: element.removeChild(divNotificacion); break;
                         }
                     }, 12000);
-                    switch(notificacionNumber[4]){
-                        case 0: parrafoContenido.textContent = "Me llamo manolo"; break;
-                        case 1: parrafoContenido.textContent = "Y me gusta el chocolate"; break;
-                        case 2: parrafoContenido.textContent = "Pasear me despeja la mente"; break;
-                        case 3: parrafoContenido.textContent = "Y bailar menea mi cocotan"; break;
-                        case 4: parrafoContenido.textContent = "LO QUE ME GUSTA PERREAAAAAAR"; break;
+                    switch(notificacionNumber[5]){
+                        case 0: parrafoContenido.textContent = mensajesSalud[10]; break;
+                        case 1: parrafoContenido.textContent = mensajesSalud[11]; break;
+                        case 2: parrafoContenido.textContent = mensajesSalud[12]; break;
+                        // case 3: parrafoContenido.textContent = "Y bailar menea mi cocotan"; break;
+                        // case 4: parrafoContenido.textContent = "LO QUE ME GUSTA PERREAAAAAAR"; break;
                     }
                     element.appendChild(divNotificacion);
                     divNotificacion.appendChild(parrafoContenido);
@@ -253,7 +259,7 @@ function generarNotificacion(idType = ""){
 
         //Introduccion
 
-        if(maxNotiNumber[5] <= 5){
+        if(maxNotiNumber[5] <= 2){
             if(idType == ids[5]){
                 if(element.id == ids[5]){
                     const divNotificacion = document.createElement('div');
@@ -268,11 +274,11 @@ function generarNotificacion(idType = ""){
                         }
                     }, 12000);
                     switch(notificacionNumber[5]){
-                        case 0: parrafoContenido.textContent = "Me llamo manolo"; break;
-                        case 1: parrafoContenido.textContent = "Y me gusta el chocolate"; break;
-                        case 2: parrafoContenido.textContent = "Pasear me despeja la mente"; break;
-                        case 3: parrafoContenido.textContent = "Y bailar menea mi cocotan"; break;
-                        case 4: parrafoContenido.textContent = "LO QUE ME GUSTA PERREAAAAAAR"; break;
+                        case 0: parrafoContenido.textContent = mensajesSalud[0]; break;
+                        case 1: parrafoContenido.textContent = mensajesSalud[1]; break;
+                        // case 2: parrafoContenido.textContent = "Pasear me despeja la mente"; break;
+                        // case 3: parrafoContenido.textContent = "Y bailar menea mi cocotan"; break;
+                        // case 4: parrafoContenido.textContent = "LO QUE ME GUSTA PERREAAAAAAR"; break;
                     }
                     element.appendChild(divNotificacion);
                     divNotificacion.appendChild(parrafoContenido);
@@ -289,3 +295,4 @@ function generarNotificacion(idType = ""){
         }
     }
 }
+
